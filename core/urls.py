437a098d6d884b6845ca3e02db0b1242772aee37
wkauan <kauanmbomfim5@gmail.com>
2023-11-12 20:28:15 -0300

    :sparkles: feat: Novas páginas (FINALIZAR)

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
@@ -5,4 +6,12 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cadastro/',views.cadastro, name='cadastro'),
+    path('empresa/', views.empresa, name='empresa'),
+    path('login/', views.login, name='login'),
+    path('produto/', views.produto, name='produto'),
+    path('pontos/', views.pontos, name='pontos'),
+    path('ticket/', views.ticket, name='ticket'),
+    path('coleta/',views.coleta, name='coleta'),
+    path('material/',views.material, name='material'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import ClienteModel,EmpresaModel, LoginModel
+from .forms import ClienteForm,EmpresaForm, LoginForm
+from . import views
 
 # Create your views here.
 
 def index(request):
     return render(request, "index.html")
+
+def cadastro(request):
+    if request.method == 'POST':
+        form = ClienteForm(request.POST)       
+        if ClienteModel.objects.filter(cpf=form.data['cpf']).exists():
+            messages.error(request,'Este cliente já foi cadastrado!') 
+        elif len(form.data['cpf']) != 11:
+             messages.error(request,'O CPF informado é inválido!') 
+        else:
+            cliente = ClienteModel()
+            cliente.cpf = form.data['cpf']
+            cliente.nome = form.data['nome']
+            cliente.telefone = form.data['telefone']
+            cliente.endereco = form.data['endereco']
+            cliente.email = form.data['email']
+            cliente.senha = form.data['senha']
+            cliente.save()
+                      
+    return render(request, 'cadastro.html')
+
+
+def empresa(request):
+    if request.method == 'POST':
+        form = EmpresaForm(request.POST)       
+        if EmpresaModel.objects.filter(cnpj=form.data['cnpj']).exists():
+            messages.error(request,'Esta empresa já foi cadastrado!') 
+        elif len(form.data['cnpj']) != 14:
+             messages.error(request,'O CNPJ informado é inválido!') 
+        else:
+            empresa = EmpresaModel()
+            empresa.cnpj = form.data['cnpj']
+            empresa.nome = form.data['nome']
+            empresa.telefone = form.data['telefone']
+            empresa.save()
+            
+    return render(request, 'empresa.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+def pontos(request):
+    return render(request, 'index;html')
+
+def produto(request):
+    return render(request, 'index;html')
+
+def coleta(request):
+    return render(request, 'index;html')
+
+def ticket(request):
+    return render(request, 'index;html')
+
+def material(request):
+    return render(request, 'index;html')
