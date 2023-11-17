19682ef19e7fba61e81f26d22ed81c299161822c
wkauan <kauanmbomfim5@gmail.com>
2023-11-17 15:43:57 -0300

    :white_check_mark: test: Atualizando testes

--- code/core/tests.py
+++ code/core/tests.py
@@ -13,24 +13,24 @@ class odsModelsTest(TestCase):
 
     def test_criar_usuario(self):
         client = MongoClient("mongodb://localhost:27017")
-        db = client['odsdatabase']
-        collection = db['odscliente']
+        db = client['greentrade']
+        collection = db['clientes']
 
         # Cria um documento no MongoDB
-        cadastro_data = {'nome': 'Bolsonaro'}
+        cadastro_data = {'nome': 'Lucas'}
         result = collection.insert_one(cadastro_data)
         self.assertIsNotNone(result.inserted_id)
 
     def test_se_foi_criado(self):
     # Verifica se o documento existe no MongoDB
-        client = MongoClient("mongodb://localhost:27017/odsdatabase")
-        collection = client['odsdatabase']['odscliente']
+        client = MongoClient("mongodb://localhost:27017/greentrade")
+        collection = client['greentrade']['clientes']
         documents = collection.find()
         self.assertTrue(documents.count() > 0)
 
     def test_criado_somente_um(self):
         # Verifica se apenas um documento foi criado
-        client = MongoClient("mongodb://localhost:27017/odsdatabase")
-        collection = client['odsdatabase']['odscliente']
+        client = MongoClient("mongodb://localhost:27017/greentrade")
+        collection = client['greentrade']['clientes']
         documents = collection.find()
         self.assertEqual(documents.count(), 1)
